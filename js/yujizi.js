7244748c722c5fa1f77308df56faaccf5036e059
guci <guci314@gmail.com>
2022-10-18 03:01:51 +0800

    add continue play

--- js/yujizi.js
+++ js/yujizi.js
@@ -6,11 +6,18 @@ function saveState()
     var audios=document.getElementsByTagName("audio")
     for ( var i=0;i<audios.length;i++ ) {
         var id=audios[i].getAttribute("id");
-        document.cookie=id+"="+document.getElementById(id).currentTime;
+        //alert(id);
+        Cookies.set(id,document.getElementById(id).currentTime);
+        //document.cookie=id+"="+document.getElementById(id).currentTime;
+        //$("#test").append(id+"="+document.getElementById(id).currentTime+"   ");
     }    
 };
 
 function loadState(){
+    //var x=Cookies.get();
+    //alert(x)
+    //$("#test").append(document.cookie);
+    //$("#test").append("------");
     var ca = document.cookie.split(';');
     for(var i=0; i<ca.length; i++) 
     {
@@ -19,10 +26,12 @@ function loadState(){
         var name=x[0];
         var value=x[1];
         //$("#test").append(name+"="+value+"   ");
-        if (name.indexOf("audio")==0){
-            audio=document.getElementById(name);
+        //if (name.indexOf("audio")==0){
+        audio=document.getElementById(name);
+        if (audio){
             audio.currentTime=value;
-        }
+        };
+        //}
     }
 }
 
