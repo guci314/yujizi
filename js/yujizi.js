b79d380658783586542ca352145373fab86ceccc
guci <guci314@gmail.com>
2022-10-20 04:39:07 +0800

    aaa

--- js/yujizi.js
+++ js/yujizi.js
@@ -3,16 +3,13 @@ setInterval("saveState()",5000);
 
 function saveState()
 {
-    var d = new Date();
-    d.setTime(d.getTime() + (10 * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-    //var audios=document.getElementsByTagName("audio")
+    // var d = new Date();
+    // d.setTime(d.getTime() + (10 * 24 * 60 * 60 * 1000));
+    // var expires = "expires="+d.toUTCString();
     for ( var i=0;i<audioArray.length;i++ ) {
         var id=audioArray[i];
-        //alert(id);
-        //Cookies.set(id,document.getElementById(id).currentTime);
-        document.cookie=id+"="+document.getElementById(id).currentTime+ ";"+expires + ";domain=" + window.location.hostname+';path=/';
-        //$("#test").append(id+"="+document.getElementById(id).currentTime+"   ");
+        // document.cookie=id+"="+document.getElementById(id).currentTime+ ";"+expires + ";domain=" + window.location.hostname+';path=/';
+        window.localStorage.setItem(id,document.getElementById(id).currentTime);
     }    
 };
 
@@ -37,24 +34,24 @@ function loadState(){
         audios[i].addEventListener('ended',next,false);
         audioArray.push(audios[i].id);
     }
-    //var x=Cookies.get();
-    //$("#test").append(document.cookie);
-    //$("#test").append("------");
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) 
-    {
-        var c = ca[i].trim();
-        var x=c.split("=");
-        var name=x[0];
-        var value=x[1];
-        //$("#test").append(name+"="+value+"   ");
-        //if (name.indexOf("audio")==0){
-        audio=document.getElementById(name);
-        if (audio){
-            audio.currentTime=value;
-        };
-        //}
-    };
+    for ( var i=0;i<audioArray.length;i++ ) {
+        var id=audioArray[i];
+        // document.cookie=id+"="+document.getElementById(id).currentTime+ ";"+expires + ";domain=" + window.location.hostname+';path=/';
+        document.getElementById(id).currentTime=window.localStorage.getItem(id);
+    }
+    // var ca = document.cookie.split(';');
+    // for(var i=0; i<ca.length; i++) 
+    // {
+    //     var c = ca[i].trim();
+    //     var x=c.split("=");
+    //     var name=x[0];
+    //     var value=x[1];
+    //     //$("#test").append(name+"="+value+"   ");
+    //     audio=document.getElementById(name);
+    //     if (audio){
+    //         audio.currentTime=value;
+    //     };
+    // };
 
 };
 
